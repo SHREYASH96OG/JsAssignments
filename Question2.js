6a432091c1046b69bba73330e34b54ea1e67b4e8
SHREYASH GAIKWAD <gaikwadshreyash1810@gmail.com>
2023-12-08 23:04:33 +0530

    Create Question2.js

---
+++ Question2.js
@@ -0,0 +1,7 @@
+let radius = 5;  //radius of Circle in cm
+let pi = 3.14;
+area(radius,pi);  //area of Circle
+function area(radius,pi){
+console.log(3.14*radius*radius + " Is area of circle for given radius.");
+
+}
